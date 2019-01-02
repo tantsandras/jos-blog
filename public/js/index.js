2f04739617745338124203066468717a53d8fb70
Sandra <grahlsan@gmail.com>
2019-01-02 17:10:47 +0000

    blogpost slideshow function

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,27 @@
 "use strict";
-const blogpostList = document.getElementById("blogpost-list");
+// const blogpostList = document.getElementById("blogpost-list");
 
 const menuBtn = document.querySelector(".burger-menu");
 menuBtn.addEventListener("click", e => {
   e.target.closest("a").classList.toggle("open");
   document.querySelector(".about-me").classList.toggle("open");
-});
+});  
 
-
-const loginBtn = document.querySelector("#login-button");
-loginBtn.addEventListener("click", e => {
-  
-  document.location.href = '/login';
-});
+  let slide_index = 1;  
+  displaySlides(slide_index);  
+  const nextSlide = (n) => {  
+      displaySlides(slide_index += n);  
+  }  
+  const currentSlide = (n) => {  
+      displaySlides(slide_index = n);  
+  }  
+  function displaySlides(n) {  
+      let i;  
+      let slides = document.getElementsByClassName("list-item");  
+      if (n > slides.length) { slide_index = 1 }  
+      if (n < 1) { slide_index = slides.length }  
+      for (i = 0; i < slides.length; i++) {  
+          slides[i].style.display = "none";  
+      }  
+      slides[slide_index - 1].style.display = "block";  
+  }  
