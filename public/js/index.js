712bf5fe3557c0e0871812e608fe746095d5a412
Sandra <grahlsan@gmail.com>
2019-01-05 13:48:13 +0000

    added fetch blogpost function

--- public/js/index.js
+++ public/js/index.js
@@ -7,8 +7,42 @@ menuBtn.addEventListener("click", e => {
   document.querySelector(".about-me").classList.toggle("open");
 });  
 
+
+
+
+const html = item => {
+  let outputHtml = `
+  <span class="date">${item.date}</span>
+        <h4 id="post-title">${item.header}</h4>
+        <img src="${item.img_url}" alt="${item.header}" id="read-post-img">
+        <p class="read-post">${item.text}</p>
+            `;
+        console.log(outputHtml)
+        const post = document.querySelector(".post");
+        post.innerHTML = outputHtml;
+        
+      };
+
+    
+
+const allBps = [];
+fetch("/bps")
+.then(res => res.json())
+.then(data => allBps.push(...data))
+.then(() => {
+const readPost = document.URL;
+const id = readPost.split("posts/")[1];
+  console.log(id)
+const bpFind = obj => {
+  return obj.id === +id;
+};
+const bp = allBps.find(bpFind)
+html(bp);
+});
+
+
   let slide_index = 1;  
-  displaySlides(slide_index);  
+  displaySlides(slide_index); 
   const nextSlide = (n) => {  
       displaySlides(slide_index += n);  
   }  
@@ -24,7 +58,9 @@ menuBtn.addEventListener("click", e => {
           slide_index = slides.length }  
         for (let item of slides) {
         item.style.display = "none";
-        } 
+       } 
       slides[slide_index -1].style.display = "grid";
       slides[slide_index +1].style.display = "grid";
   }  
+ 
+
