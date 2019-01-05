c246e19409947c305edd4e123eb40a54c8b92177
Sandra <grahlsan@gmail.com>
2019-01-05 13:48:49 +0000

    changed auth

--- src/app.js
+++ src/app.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '/write');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-type: Authorization');
   next();
 });
 
