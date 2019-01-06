dfd18d87aece7a53326a70aea4d8efce64342791
Sandra <grahlsan@gmail.com>
2019-01-06 19:00:07 +0000

    added more regex

--- src/views/helpers/trim.js
+++ src/views/helpers/trim.js
@@ -1,6 +1,6 @@
 const preview = (str) => {
 
-    let preveiw = str.replace(/<[^>]*>/g, "");
+    let preveiw = str.replace(/(&nbsp;)|(<[^>]*>)/g, "");
     return preveiw.substring(0, 200) + ' ...';
 };
 
